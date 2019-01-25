12b3ff38ca7b77a95347443e7401a274b2b196c1
shoppingzh <1049262057@qq.com>
2019-01-25 16:10:37 +0800

    hello world例程

---
+++ hello/hello/HelloUI.h
@@ -0,0 +1,13 @@
+//
+//  HelloUI.h
+//  hello
+//
+//  Created by dascomsoft on 2019/1/25.
+//  Copyright © 2019年 Littlezheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloUI : UIViewController
+
+@end
---
+++ hello/hello/HelloUI.m
@@ -0,0 +1,37 @@
+//
+//  HelloUI.m
+//  hello
+//
+//  Created by dascomsoft on 2019/1/25.
+//  Copyright © 2019年 Littlezheng. All rights reserved.
+//
+
+#import "HelloUI.h"
+
+@interface HelloUI ()
+
+@end
+
+@implementation HelloUI
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
